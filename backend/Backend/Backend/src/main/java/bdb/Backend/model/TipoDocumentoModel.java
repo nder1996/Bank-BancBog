a708b12aba7f6cd19a337576c765177fe5b49e07
aarevalom <ander_2030@hotmail.com>
2024-12-13 07:28:33 -0500

    correccion lombok de tipo de documento

--- backend/Backend/Backend/src/main/java/bdb/Backend/model/TipoDocumentoModel.java
+++ backend/Backend/Backend/src/main/java/bdb/Backend/model/TipoDocumentoModel.java
@@ -4,13 +4,65 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class TipoDocumentoModel {
 
-    String codigo;
-    String descripcion;
+    private String codigo;
+    private String descripcion;
+
+    // Constructor sin argumentos
+    public TipoDocumentoModel() {
+    }
+
+    // Constructor con argumentos
+    public TipoDocumentoModel(String codigo, String descripcion) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+    }
+
+    // Getters y Setters
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    // Método equals
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TipoDocumentoModel that = (TipoDocumentoModel) o;
+        return Objects.equals(codigo, that.codigo) &&
+                Objects.equals(descripcion, that.descripcion);
+    }
+
+    // Método hashCode
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo, descripcion);
+    }
+
+    // Método toString
+    @Override
+    public String toString() {
+        return "TipoDocumentoModel{" +
+                "codigo='" + codigo + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                '}';
+    }
 
 
 }
